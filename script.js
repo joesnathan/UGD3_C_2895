0b7ebb30078456fb070836cc164ae6ccb6d0d4c8
joesnathan <Jonathanindrawan224@gmail.com>
2026-03-05 18:10:49 +0700

    tes 1

---
+++ script.js
@@ -0,0 +1,18 @@
+const form = document.getElementById("loginForm");
+
+form.addEventListener("submit", function(event){
+
+    event.preventDefault();
+
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+    let errorMsg = document.getElementById("errorMsg");
+
+if(username === "" || password === ""){
+    errorMsg.innerText = "Username atau Password tidak boleh kosong!";
+    errorMsg.style.fontSize = "13px";
+    errorMsg.style.color = "red";
+}
+else if(username === "jo" && password === "2895"){
+    window.location.href = "https://instagram.com/jnthn.indra";
+}});
