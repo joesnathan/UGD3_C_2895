4400bf3de7212ecd90a9346e1b1ca596f61d0efc
joesnathan <Jonathanindrawan224@gmail.com>
2026-03-10 16:27:08 +0700

    biar bonus

--- script.js
+++ script.js
@@ -12,6 +12,13 @@ if(username === "" || password === ""){
     errorMsg.innerText = "Username atau Password tidak boleh kosong!";
     errorMsg.style.fontSize = "13px";
     errorMsg.style.color = "red";
-} else {
+}
+else if (password.length < 8 || password.length > 16){ 
+    errorMsg.innerText = "Panjang password harus antara 8-16 karakter!";
+    errorMsg.style.fontSize = "13px";
+    errorMsg.style.color = "red";
+}
+
+else {
     window.location.href = "https://instagram.com/jnthn.indra";
 }});
